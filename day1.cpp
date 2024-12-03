529ee8b6e49ccf4efbc248197badbe6ffb2cabc8
Saphio <106698837+Saphio@users.noreply.github.com>
2024-12-02 16:30:08 -0800

    update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -1,12 +1,13 @@
-// Online C++ compiler to run C++ program online
+// DAY 1: PART 1
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
-// #include <fstream>
-
-// ifstream fin("input1.txt");
+#include <fstream>
 
 using namespace std;
+ifstream fin("day1.txt");
+
 
 vector<int> a;
 vector <int> b;
@@ -14,11 +15,13 @@ vector <int> b;
 int main() {
     int i;
     int j;
-    cin >> i >> j;
-    while (i != -1) {
+    // cin >> i >> j;
+    fin >> i >> j;
+    while (!fin.eof()) {
         a.push_back(i);
         b.push_back(j);
-        cin >> i >> j;
+        // cin >> i >> j;
+        fin >> i >> j;
     }
     
     sort(a.begin(), a.end());
